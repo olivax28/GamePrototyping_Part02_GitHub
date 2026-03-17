25fd0277fcf30a316bedd42c1be53d841ac36f94
olivax28 <olivax28@outlook.com>
2026-03-17 17:31:21 -0400

    draggable windows implemented

---
+++ game_prototype_final/Assets/NewScripts/DragWindow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragWindow : MonoBehaviour, IDragHandler
+{
+    [SerializeField] private RectTransform dragRectTransform;
+       public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += eventData.delta;
+      
+      
+    }
+}
