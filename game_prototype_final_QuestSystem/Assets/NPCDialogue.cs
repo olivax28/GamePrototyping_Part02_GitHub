92c48ad05d987125b9d59c1462e4ed01ea45e169
juliax28 <juliax28@outlook.com>
2026-04-04 00:36:25 -0400

    new NPC Dialogue interaction with writing characters and UI

--- game_prototype_final_QuestSystem/Assets/NPCDialogue.cs
+++ game_prototype_final_QuestSystem/Assets/NPCDialogue.cs
@@ -12,5 +12,6 @@ public class NPCDialogue : ScriptableObject
     public float typingSpeed = 0.05f;
     public bool[] autoProgressLines;
     public float autoProgressDelay = 1.5f;
-
+    public AudioClip voiceSound;
+    public Sprite npcPortrait;
 }
--- game_prototype_final_QuestSystem/Assets/NewScripts/NPCInteractable.cs
+++ game_prototype_final_QuestSystem/Assets/NewScripts/NPCInteractable.cs
@@ -1,4 +1,8 @@
 using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class NPCInteractable : MonoBehaviour, IInteractable
@@ -10,8 +14,91 @@ public class NPCInteractable : MonoBehaviour, IInteractable
         return interactText;
     }
 
+    public NPCDialogue dialogueData;
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
+    
     public void Interact(Transform interactorTransform)
     {
-        ChatBubble.Create(transform.transform, new Vector3(-1f,3f,0f), "here is a test!");
+        if (dialogueData == null)
+            return;
+    
+    
+        if (isDialogueActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialogue();
+        }
+    }
+
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.npcName);
+        portraitImage.sprite = dialogueData.npcPortrait;
+
+        dialoguePanel.SetActive(true);
+
+        StartCoroutine(TypeLine());
+
+    }
+
+    void NextLine()
+    {
+        if (isTyping)
+        {
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if (++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+
+
+        foreach(char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+
+        }
+        isTyping = false;
+
+        if(dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.autoProgressDelay);
+            NextLine();
+        }
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+        
     }
 }
+
