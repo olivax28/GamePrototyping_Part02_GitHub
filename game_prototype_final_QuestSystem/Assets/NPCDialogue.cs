3a6b52780829de01ccf45eb3f1f9782eadb4ec12
juliax28 <juliax28@outlook.com>
2026-04-03 23:15:46 -0400

    restart Diaogue and NPC scripting

--- game_prototype_final_QuestSystem/Assets/NPCDialogue.cs
+++ game_prototype_final_QuestSystem/Assets/NPCDialogue.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NPCDialogue", menuName = "NPCDialogue")]
+[CreateAssetMenu(fileName = "NewNPCDialogue", menuName = "NPCDialogue")]
 public class NPCDialogue : ScriptableObject
 
 {
     public string npcName;
     public string[] dialogueLines;
     public float typingSpeed = 0.05f;
-    public bool[] autoprogressLines;
-    public float autoprogressDelay = 1.5f;
+    public bool[] autoProgressLines;
+    public float autoProgressDelay = 1.5f;
 
 }
