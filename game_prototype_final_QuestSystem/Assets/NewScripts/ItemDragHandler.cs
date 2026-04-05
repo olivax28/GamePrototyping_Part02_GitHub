b687440bd6c936cc947528705e830ca6d94bdff3
olivax28 <olivax28@outlook.com>
2026-04-04 22:32:22 -0400

    fixed bugged inventory

--- game_prototype_final_QuestSystem/Assets/NewScripts/ItemDragHandler.cs
+++ game_prototype_final_QuestSystem/Assets/NewScripts/ItemDragHandler.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystem;
+using UnityEngine.EventSystems;
 
 public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
@@ -17,18 +17,18 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     {
         originalParent = transform.parent;
         transform.SetParent(transform.root);
-        canvasGroup.blockRaycasts = false;
+        canvasGroup.blocksRaycasts = false;
         canvasGroup.alpha = 0.6f;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        transform.positon = eventData.position;
+        transform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        canvasGroup.blockRaycasts = true;
+        canvasGroup.blocksRaycasts = true;
         canvasGroup.alpha = 1f;
 
         Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>();
